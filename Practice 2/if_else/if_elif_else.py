8fa3af0f7d63cdb0eabb177892c6ddb0f09795fc
n3llex <altynbekaslan5@gmail.com>
2026-02-09 21:33:42 +0500

    added if elif else

---
+++ Practice 2/if_else/if_elif_else.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+x = 30
+
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
