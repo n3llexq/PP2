4722ae25479782c2c4e358a14a41b05ee8a34d57
n3llex <altynbekaslan5@gmail.com>
2026-02-09 21:11:22 +0500

    add boolean intro

---
+++ Practice 2/boolean/boolean_intro.py
@@ -0,0 +1,25 @@
+coding = True
+cheating = False
+
+print(coding)
+print(cheating)
+
+print(bool("Hello"))
+print(bool(42))
+print(bool(["Python"]))
+
+print(bool(False)) 
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool([]))
+
+is_even = False
+a = 4
+b = 3
+if a % 2 == 0:
+    is_even = True
+print(is_even)
+
+if b % 2 != 0:
+    is_even = False
