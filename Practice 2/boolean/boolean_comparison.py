37fae1e567889a85d7589f3fc686f16759acb5f5
n3llex <altynbekaslan5@gmail.com>
2026-02-09 21:21:44 +0500

    added boolean comparison

---
+++ Practice 2/boolean/boolean_comparison.py
@@ -0,0 +1,16 @@
+x = 10
+y = 20
+
+print(x == 10)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= 10)
+print(y <= 20)
+
+a = "Apple"
+b = "Banana"
+
+print(a == "Apple")
+print(a < b)
+print(10 == "10")
