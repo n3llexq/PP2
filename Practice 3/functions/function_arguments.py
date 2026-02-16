c17a36c1201f4943d5e03d4bb28e4bbaf6f18c6a
n3llex <altynbekaslan5@gmail.com>
2026-02-16 19:40:16 +0500

    added function_arguments

---
+++ Practice 3/functions/function_arguments.py
@@ -0,0 +1,8 @@
+def create_character(name, role="Warrior", level=1):
+    print(f"Character: {name}")
+    print(f"Class: {role}")
+    print(f"Level: {level}")
+
+create_character("Aragorn")
+create_character("Gandalf", "Wizard", 100)
+create_character("Legolas", role="Archer")
