08302548ad0eab00f60f71293e7a43c5ec2f8c4f
n3llex <altynbekaslan5@gmail.com>
2026-02-16 20:20:29 +0500

    added class methods

---
+++ Practice 3/classes/class_methods.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. Remaining: {self.balance}")
+
+acc = BankAccount("Bruce Wayne", 10000)
+acc.deposit(50000)
+acc.withdraw(60000)
+acc.withdraw(60001)
