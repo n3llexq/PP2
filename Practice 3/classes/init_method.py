05e2b37071fb1ca0f0d710c742340521346138f0
n3llex <altynbekaslan5@gmail.com>
2026-02-16 20:12:39 +0500

    added init method

---
+++ Practice 3/classes/init_method.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+person1 = Person("Alice", 30)
+person2 = Person("Bob", 25)
+
+print(person1.name)
+print(person1.age)
+
+print(person2.name)
+print(person2.age)
