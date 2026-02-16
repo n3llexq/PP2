6bd999c503ec49aba718573be0f28a29fffa591f
n3llex <altynbekaslan5@gmail.com>
2026-02-16 19:57:27 +0500

    added lambda basics

---
+++ Practice 3/lambda/lambda_basics.py
@@ -0,0 +1,10 @@
+add = lambda a, b: a + b
+multiply = lambda a, b: a * b
+square = lambda x: x ** 2
+
+print(add(5, 3))
+print(multiply(4, 6))
+print(square(10))
+
+greeting = lambda name: f"Hello, {name}!"
+print(greeting("Neo"))
